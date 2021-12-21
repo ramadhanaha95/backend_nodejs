ebd6b85f33ad6e4cfdb85c1e6c248a203fcb82c9
ap699285 <ap699285@gmail.com>
2021-12-21 16:19:25 +0700

    update_new

--- controllers/UploadController.js
+++ controllers/UploadController.js
@@ -1,113 +1,114 @@
 import {
-    DB,
-    DB2
+    MYSQL,
+    SQLSRV
 } from '../config/database.js'
-import bcrypt from 'bcrypt'
 import resp_code from '../src/libs/response_code.js'
-
-//this is set of data to encrypt or decrypt data
-import {
-    SetResponseJsonEncrypt
-} from '../src/libs/response_encrypt.js'
-import {
-    SetResponseJsonDecrypt
-} from '../src/libs/response_decrypt.js'
-
-//this is to get jwt token
-import {
-    GetJwtToken
-} from '../src/auth/jwt/useJwt.js'
-import {
-    login_validation,
-    register_validation
-} from '../src/validations/form_validation.js'
-
 import dotenv from 'dotenv'
 
 //Required when use File Upload
+import FTPClient from 'ftp'
 import fs from 'fs'
 import formidable from 'formidable'
 
 import {
-    ftp_client,
     ftp1
 } from '../config/filestorage.js'
+import console from 'console'
 
 dotenv.config()
 
 //Single File
-function Upload(req, res) {
+export function Upload(req, res) {
+    let dt = new Date()
+    let dtf = dt.getTime()
+    let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let nf = ''
+    for (let i = 0; i < 15; i++) {
+        nf += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    let nama_file = nf + dtf
+
     let form = new formidable.IncomingForm()
     form.parse(req, function (err, fields, files) {
-        var newpath = '/' + files.image.originalFilename;
-
-        fs.readFile(files.image.filepath, (err, result) => {
-            ftp_client.connect(ftp1);
-            ftp_client.on('ready', function () {
-                ftp_client.put(result, newpath, function (err) {
+        let arr_filename = files.image.originalFilename.split(".")
+        let image_ext = arr_filename[arr_filename.length - 1]
+        let newpath = nama_file + "." + image_ext;
+        fs.readFile(files.image.filepath, async (err, result) => {
+            const ftp_client = await new FTPClient();
+            await ftp_client.connect(ftp1);
+            await ftp_client.on('ready', function () {
+                ftp_client.put(result, newpath,async function (err) {
                     if (err) {
                         return res.json(err)
                     } else {
+                        const db = await MYSQL()
                         try {
-                            DB2.beginTransaction()
-                            var query = `INSERT INTO file_upload (user_id, file) VALUES (?,?)`;
-                            DB2.query(query, [req.user.id, files.image.originalFilename], (err, result) => {
+                            db.beginTransaction()
+                            let query = `INSERT INTO file_upload (user_id, file) VALUES (?,?)`;
+                            db.query(query, [req.user.id, newpath], (err, result) => {
                                 if (err) {
-                                    DB2.rollback(() => {
-                                        return res.json(err)
+                                    db.rollback(() => {
+                                    return res.json(err)
                                     });
                                 } else {
-                                    DB2.commit()
-                                    return res.json(resp_code[0])
+                                    db.commit()
                                 }
                             })
                         } catch (err) {
-                            DB2.rollback(() => {
+                            await db.rollback(() => {
                                 return res.json(err)
                             });
                         }
                     }
-                    ftp_client.end();
                 });
             });
         })
     });
+    return res.json(resp_code[4])
 }
 
 //Multiple File
-function Upload2(req, res) {
-    ftp_client.connect(ftp1);
-
-    let form = new formidable.IncomingForm()
-    form.parse(req, function (err, fields, files) {
-        // for (let i = 0; i < files.length; i++){
-        //     return res.json(files[i].image)
-        // }
-        var data_image = JSON.parse(JSON.stringify(form.openedFiles))
-        data_image.forEach(data => {
-
-            var newpath = '/' + data.originalFilename;
-            fs.readFile(data.filepath, (err, result) => {
-                ftp_client.on('ready', function () {
-                    ftp_client.put(result, newpath, function (err) {
-                        if (err) {
-                            return res.json(err)
-                        }
+export async function Upload2(req, res) {
+    try{
+        let form = new formidable.IncomingForm()
+        await form.parse(req, function (err, fields, files) {
+            // for (let i = 0; i < files.length; i++){
+            //     return res.json(files[i].image)
+            // }
+            let data_image = JSON.parse(JSON.stringify(form.openedFiles))
+            data_image.forEach(data => {
+                let dt = new Date()
+                let dtf = dt.getTime()
+                let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                let nf = ''
+                for (let i = 0; i < 15; i++) {
+                    nf += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+                }
+                let nama_file = nf + dtf
+                let arr_filename = data.originalFilename.split(".")
+                let image_ext = arr_filename[arr_filename.length - 1]
+    
+                let newpath = nama_file + "." + image_ext;
+                fs.readFile(data.filepath, async (err, result) => {
+                    const ftp_client = await new FTPClient();
+                    await ftp_client.connect(ftp1);
+                    await ftp_client.on('ready', function () {
+                        ftp_client.put(result, newpath, function (err) {
+                            if (err) {
+                                return res.json(err)
+                            }
+                        });
                     });
-                });
+                    //await ftp_client.end();
+                })
             })
-
         })
-        //ftp_client.end();
-        return res.json("berhasil")
-    })
+        return res.json(resp_code[4])
+    }catch(e){
+        console.log(e)
+    }
 }
 
 //export fuction bisa di taruh di bawah
 //atau di taruh di depan function
 //ex: export const <nama function>
-
-export {
-    Upload,
-    Upload2
-}
