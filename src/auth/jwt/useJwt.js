25ace469cff03021a225748c05f7ed6d4d0d2932
bznid404 <79962321+bznid404@users.noreply.github.com>
2021-12-25 16:54:03 +0700

    redis_auto_change_when_down

--- src/auth/jwt/useJwt.js
+++ src/auth/jwt/useJwt.js
@@ -1,71 +1,63 @@
 import Jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
-import {
-    client
-} from '../../../src/libs/redis/connect.js'
 dotenv.config()
+import {
+    createClient
+} from "redis"
+import {
+    GetJwtFromRedis,
+    PushJwtToRedis,
+    DestroyJwtFromRedis
+} from '../../libs/redis/redis_auth.js'
 
-export const GetJwtToken = (id, role_id, email_verification_status) => {
-    const token = Jwt.sign({
+
+//Auth Dengan Redis Apabila Server Redis Sedang Uptime
+export const GetJwtTokenRedis = async (id, role_id, email_verification_status) => {
+    const get_token_from_redis = await GetJwtFromRedis(id)
+    if (get_token_from_redis == 'redis_error') {
+        return get_token_from_redis
+    } else {
+        if (get_token_from_redis != null) {
+            let data_token = {
+                token: get_token_from_redis,
+                msg: "Redis is Live"
+            }
+            return data_token
+        } else {
+            const token = await Jwt.sign({
+                id: id,
+                role_id: role_id,
+                email_verification_status: email_verification_status
+            }, process.env.TOKEN_SECRET, {
+                expiresIn: '100h'
+            });
+            await PushJwtToRedis(id, token)
+            let data_token = {
+                token: token,
+                msg: "Redis is Live and Succesuly Push Token"
+            }
+            return data_token
+        }
+    }
+}
+
+//Auth Tanpa Redis Apabila Server Redis Sedang Downtime (Perpindahan Secara Otomatis)
+export const GetJwtToken = async (id, role_id, email_verification_status) => {
+    const token = await Jwt.sign({
         id: id,
         role_id: role_id,
         email_verification_status: email_verification_status
     }, process.env.TOKEN_SECRET, {
         expiresIn: '100h'
     });
-
-    return token
+    let data_token = {
+        token: token,
+        msg: "Redis is Down"
+    }
+    return data_token
 }
-
-export const VerifyToken = (req, res, next) => {
-    const theToken =
-        req.body.token || req.query.token || req.headers.token;
-
-    if (!theToken) {
-        return res.status(403).json("A token is required for authentication")
-    }
-    try {
-        const decoded = Jwt.verify(theToken, process.env.TOKEN_SECRET)
-        req.user = decoded
-        if (decoded.email_verification_status == 1) {
-            let data = {
-                msg: "Please check and verify the email first",
-                user_id: decoded.id
-            }
-            return res.status(403).json(data)
-        }
-    } catch (err) {
-        return res.status(401).json("Invalid Token")
-    }
-    return next();
-};
 
 //JWT MENGGUNAKAN REDIS
-
-export const GetJwtTokenRedis = async (id, role_id, email_verification_status) => {
-    const get_token_from_redis = await client.get("user_id_redis_"+id.toString())
-    if (get_token_from_redis != null) {
-        return get_token_from_redis
-    } else {
-        const token = await Jwt.sign({
-            id: id,
-            role_id: role_id,
-            email_verification_status: email_verification_status
-        }, process.env.TOKEN_SECRET, {
-            expiresIn: '100h'
-        });
-        await client.set("user_id_redis_"+id.toString(), token, 'EX', 365 * 24 * 60 * 60, (err, reply) => {
-            if (err) {
-                console.log(err.message)
-                reject(createError.InternalServerError())
-                return
-            }
-            resolve(reply)
-        })
-
-        return token
-    }
-}
 
 export const VerifyTokenRedis = async (req, res, next) => {
     const theToken =
@@ -76,28 +68,57 @@ export const VerifyTokenRedis = async (req, res, next) => {
     try {
         const decoded = await Jwt.verify(theToken, process.env.TOKEN_SECRET)
         req.user = decoded
-        const get_token_from_redis = await client.get("user_id_redis_"+decoded.id.toString())
-        if (get_token_from_redis == theToken) {
-            if (decoded.email_verification_status == 1) {
+        const get_token_from_redis = await GetJwtFromRedis(decoded.id)
+
+        if (get_token_from_redis != 'redis_error') {
+            if (get_token_from_redis == theToken) {
+                if (decoded.email_verification_status == 1) {
+                    let data = {
+                        msg: "Please check and verify the email first",
+                        user_id: decoded.id
+                    }
+                    return res.status(403).json(data)
+                }
+            } else {
                 let data = {
-                    msg: "Please check and verify the email first",
-                    user_id: decoded.id
+                    msg: "Please Re Login"
                 }
                 return res.status(403).json(data)
             }
+            return next();
         } else {
-            let data = {
-                msg: "Please Re Login"
-            }
-            return res.status(403).json(data)
+            return next();
         }
+        
     } catch (err) {
         return res.status(401).json("Invalid Token")
     }
-    return next();
 }
 
-export const JwtRedisLogout = async (payload) => {
-    await client.DEL("user_id_redis_"+payload.toString())
-    return "Logout Success"
+export const JwtLogoutRedis = async (payload) => {
+    const DestroyJWT = await DestroyJwtFromRedis(payload)
+    return DestroyJWT
 }
+
+// export const VerifyToken = async (req, res, next) => {
+//     const theToken =
+//         req.body.token || req.query.token || req.headers.token;
+
+//     if (!theToken) {
+//         return res.status(403).json("A token is required for authentication")
+//     }
+//     try {
+//         const decoded = await Jwt.verify(theToken, process.env.TOKEN_SECRET)
+//         req.user = decoded
+//         if (decoded.email_verification_status == 1) {
+//             let data = {
+//                 msg: "Please check and verify the email first",
+//                 user_id: decoded.id
+//             }
+//             return res.status(403).json(data)
+//         }
+//     } catch (err) {
+//         return res.status(401).json("Invalid Token")
+//     }
+//     return next();
+// };
